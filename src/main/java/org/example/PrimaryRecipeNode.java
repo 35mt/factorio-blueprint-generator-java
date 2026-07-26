3469d836d36b64a0356c185df7dbbbef81e87a44
35mt <35mt35mt35mt@gmail.com>
2026-07-27 01:14:47 +0500

    PrimaryRecipeNode

--- src/main/java/org/example/PrimaryRecipeNode.java
+++ src/main/java/org/example/PrimaryRecipeNode.java
@@ -2,25 +2,33 @@ package org.example;
 
 import org.example.read.Recipe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimaryRecipeNode {
     final private String name;
-    final private String parent;
     final private double needPerSecond;
     final private Recipe recipe;
+    final private PrimaryRecipeNode parent;
+    final private List<PrimaryRecipeNode> children = new ArrayList<>();
 
-    public PrimaryRecipeNode(String name, String parent, double needPerSecond, Recipe recipe) {
+    public PrimaryRecipeNode(String name, double needPerSecond, Recipe recipe, PrimaryRecipeNode parent) {
         this.name = name;
-        this.parent = parent;
         this.needPerSecond = needPerSecond;
         this.recipe = recipe;
+        this.parent = parent;
+    }
+
+    public PrimaryRecipeNode getParent() {
+        return parent;
+    }
+
+    public List<PrimaryRecipeNode> getChildren() {
+        return children;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getParent() {
-        return parent;
     }
 
     public double getNeedPerSecond() {
